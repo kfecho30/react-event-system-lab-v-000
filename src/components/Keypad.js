a003d53997d409cf8d35994d4e18642a6424c77b
Kelsey Fecho <kfecho30@gmail.com>
2018-07-24 13:25:25 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,7 +5,10 @@ export default class Keypad extends React.Component{
 
   render() {
     return(
-      
+      <form>
+        <input type='password'></input>
+        <input type='submit'/>
+      </form>
     )
   }
 }
